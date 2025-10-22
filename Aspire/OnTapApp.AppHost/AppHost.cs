130b74fbc2600771efd442a047f8daf2ac486118
Leonardo Porto <leokporto@protonmail.com>
2025-10-21 21:34:48 -0300

    Add PostgreSQL and Keycloak integration
    Integrated PostgreSQL with secure credentials and connection string.
    Configured Keycloak for authentication and linked it to the API.
    Updated `OnTapApp_API` and `OnTapApp_Web` to reference Keycloak.
    Enabled user secrets in multiple projects for secure configuration.
    Added `BeersDbConnection` to `appsettings` for database setup.
    Updated dependencies to include `Aspire.Hosting.Keycloak`.

--- Aspire/OnTapApp.AppHost/AppHost.cs
+++ Aspire/OnTapApp.AppHost/AppHost.cs
@@ -3,18 +3,29 @@ using System.Runtime.InteropServices;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var pgsqlUser = builder.AddParameter("username", secret: true);
+var pgsqlPassword = builder.AddParameter("password", secret: true);
 
+var beersDbConnection = builder.AddConnectionString("BeersDbConnection");
 
-//var postgres = builder.AddPostgres("postgres");
-//var beersDb = postgres.AddDatabase("beersdb","beers_db");
+var postgres = builder.AddPostgres("postgres", pgsqlUser, pgsqlPassword)
+    .WithDataVolume()
+    .WithPgWeb();
 
+var beersDb = postgres.AddDatabase("beersdb","beers_db");
 
-var api = builder.AddProject<Projects.OnTapApp_API>("ontapapp-api");
+var keycloak = builder.AddKeycloak("keycloak", 8080)
+    .WithDataVolume();
+
+var api = builder.AddProject<Projects.OnTapApp_API>("ontapapp-api")
+    .WithReference(keycloak)
+    .WaitFor(keycloak);
     //.WithReference(beersDb);
 
 builder.AddProject<Projects.OnTapApp_Web>("ontapapp-web")
     .WithExternalHttpEndpoints()
-    .WithReference(api)
+    .WithReference(keycloak)
+    .WithReference(api)    
     .WaitFor(api);
 
 
