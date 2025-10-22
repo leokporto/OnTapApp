b3c303b30ba4491997e333d99e02dbf210665e41
Leonardo Porto <leokporto@protonmail.com>
2025-10-22 07:34:58 -0300

    Update PostgreSQL config and add useful commands doc
    Updated PostgreSQL parameter names for clarity and explicitly set data volume names and container lifetimes for PostgreSQL and Keycloak. Added a new `UsefulCommands.md` file documenting PostgreSQL backup and restore commands for Dockerized environments. Included a commented-out alternative configuration for `beersDb` and retained an unused reference to it in the API configuration.

--- Aspire/OnTapApp.AppHost/AppHost.cs
+++ Aspire/OnTapApp.AppHost/AppHost.cs
@@ -3,19 +3,23 @@ using System.Runtime.InteropServices;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var pgsqlUser = builder.AddParameter("username", secret: true);
-var pgsqlPassword = builder.AddParameter("password", secret: true);
+var pgsqlUser = builder.AddParameter("postgresql-username", secret: true);
+var pgsqlPassword = builder.AddParameter("postgresql-password", secret: true);
 
 var beersDbConnection = builder.AddConnectionString("BeersDbConnection");
 
 var postgres = builder.AddPostgres("postgres", pgsqlUser, pgsqlPassword)
-    .WithDataVolume()
+    .WithDataVolume("beersdb")
+    .WithLifetime(ContainerLifetime.Persistent)
     .WithPgWeb();
 
 var beersDb = postgres.AddDatabase("beersdb","beers_db");
+//var beersDb = builder.AddContainer("", "")
+//    .WithEnvironment("POSTGRES_USER", pgsqlUser);
 
 var keycloak = builder.AddKeycloak("keycloak", 8080)
-    .WithDataVolume();
+    .WithDataVolume("keycloak")
+    .WithLifetime(ContainerLifetime.Persistent);
 
 var api = builder.AddProject<Projects.OnTapApp_API>("ontapapp-api")
     .WithReference(keycloak)
---
+++ Docs/UsefulCommands.md
@@ -0,0 +1,19 @@
+# Useful commands
+
+This document serves as a second memory to help remember useful commands.
+
+## PgSql Commands
+
+### Backup database
+
+`docker exec -e PGPASSWORD=<password> BeersDb_PgSql pg_dump -h localhost -U <username> -F c -b -v -f /var/lib/postgresql/data/beers_db.backup beers_db`
+
+### Restore database
+
+**Create Database**
+
+`docker exec -e PGPASSWORD=<password> NomeDoContainer psql -U <username> -c "CREATE DATABASE beers_db;"`
+
+**Restore data**
+
+`docker exec -e PGPASSWORD=<password> NomeDoContainer pg_restore -U <username> -d beers_db -v /var/lib/postgresql/data/beers_db.backup;"`
